9818b2889146c0f5b83fc607a50cc153cc879f75
Phumelela <phumelela@Phumelelas-MacBook-Air.local>
2025-02-17 00:54:49 +0200

    added billboard call

--- music.py
+++ music.py
@@ -1,2 +1,20 @@
-def get_music_data():
-    pass
+import http.client
+import date
+
+today = date.get_today_date()
+print(today[0])
+past = date.get_future_date(-2)
+
+conn = http.client.HTTPSConnection("billboard-api2.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "api-key",
+    'x-rapidapi-host': "billboard-api2.p.rapidapi.com",
+}
+
+conn.request("GET", f"/hot-100?date={past}&range=1-10", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
